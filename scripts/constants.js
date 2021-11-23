4b11399d122d69586cbda2e6900c9cd57653a507
Emmanuel <emmopk11@outlook.com>
2021-11-23 18:50:07 +0100

    Add animation to tetrominoes

--- scripts/constants.js
+++ scripts/constants.js
@@ -7,6 +7,23 @@ const KEY = {
   UP: 38,
   RIGHT: 39,
   DOWN: 40,
+  SPACE: 32,
 };
+
+const COLORS = ["cyan", "blue", "orange", "yellow", "green", "purple", "red"];
+
+const SHAPES = [
+  [
+    [0, 0, 0, 0],
+    [1, 1, 1, 1],
+    [0, 0, 0, 0],
+    [0, 0, 0, 0],
+  ],
+  [
+    [2, 0, 0],
+    [2, 2, 2],
+    [0, 0, 0],
+  ],
+];
 
 Object.freeze(KEY);
--- scripts/main.js
+++ scripts/main.js
@@ -5,17 +5,39 @@ ctx.canvas.width = COLS * BLOCK_SIZE;
 ctx.canvas.height = ROWS * BLOCK_SIZE;
 
 ctx.scale(BLOCK_SIZE, BLOCK_SIZE);
-let board;
+let board = new Board(ctx);
+
+// ========= Methods ================
+let requestId;
 
 function play() {
-  board = new Board(ctx);
-  draw();
+  addEventListener();
+
+  let piece = new Piece(ctx);
+
+  board.piece = piece;
+
+  if (requestId) {
+    cancelAnimationFrame(requestId);
+  }
+
+  time.start = performance.now();
+  animate();
 }
 
 function draw() {
   const { width, height } = ctx.canvas;
   ctx.clearRect(0, 0, width, height);
   board.piece.draw();
+}
+
+function drop() {
+  let p = moves[KEY.DOWN](board.piece);
+
+  if (board.valid(p)) {
+    board.piece.move(p);
+    draw();
+  }
 }
 
 // ========= Moves ================
@@ -25,20 +47,52 @@ const moves = {
   [KEY.RIGHT]: (p) => ({ ...p, x: p.x + 1 }),
   [KEY.DOWN]: (p) => ({ ...p, y: p.y + 1 }),
   [KEY.UP]: (p) => board.rotate(p),
+  [KEY.SPACE]: (p) => ({ ...p, y: p.y + 1 }),
 };
+
+//=========== Animation ===========/
+
+let time = { start: 0, elapsed: 0, level: 1000 };
+
+function animate(now = 0) {
+  time.elapsed = now - time.start;
+
+  if (time.elapsed > time.level) {
+    time.start = now;
+    drop();
+  }
+
+  ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
+
+  board.piece.draw();
+
+  requestId = requestAnimationFrame(animate);
+}
 
 // ========= Events ================
 
-document.addEventListener("keydown", (event) => {
-  event.preventDefault();
+function addEventListener() {
+  document.removeEventListener("keydown", handleKeyPress);
+  document.addEventListener("keydown", handleKeyPress);
+}
 
-  if (moves[event.keyCode]) {
-    let p = moves[event.keyCode](board.piece);
+function handleKeyPress(evt) {
+  evt.preventDefault();
 
-    board.piece.move(p);
+  if (moves[evt.keyCode]) {
+    let p = moves[evt.keyCode](board.piece);
 
-    draw();
+    if (evt.keyCode === KEY.SPACE) {
+      while (board.valid(p)) {
+        board.piece.move(p);
+
+        p = moves[KEY.SPACE](board.piece);
+      }
+    }
+
+    if (board.valid(p)) {
+      board.piece.move(p);
+      draw();
+    }
   }
-
-  return false;
-});
+}
